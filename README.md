b80f94721c04627a334ceb1d4390793a225172dd
Zachary Davis <zdavis1995@gmail.com>
2018-10-07 09:47:26 -0400

    Updated README, small changes to source.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Rothko_Bot
 
-This bot creates images using PIL that are visually similar to the works of artist Mark Rothko. 
+This bot creates images using the Python Imaging Library. These images are visually similar to the works of abstract artist Mark Rothko (1903-1970). 
 Be sure to follow Rothko_Bot on Twitter! https://twitter.com/Rothko_Bot
 
 The conversion methods from hex to RGB and the file containing the color selections are from here: https://github.com/joemfox/colorschemer
@@ -16,12 +16,12 @@ If you would like to use this bot for your own Twitter account, follow these ste
 5. Include your own API and Access keys in the .py file (found on the site above)
 6. Run `python RothkoBot.py`
 
-The source code is very customizable and has many capabilites using the PIL library.
+The source code is customizable to your own wants or needs. Any additions built upon the Python Imaging Library should work just fine!
 
 ## Examples of Rothko_Bot's Creations
 ![alt text](https://github.com/ZacharyDavis/RothkoBot/blob/master/Images/Rothko.gif)
 
 ## Problems Solved (or Circumvented)
-- Implemented a running count of how many images have been tweeted out by Rothko_Bot. This was done using an external text file to keep track between between program termination and recompile. Not the most elegant solution I'm sure, but it works...
+- Implemented a running count of how many images have been tweeted out by Rothko_Bot. This was done using an external text file to keep track of the current tweet number to be loaded into the program when it is re-run after being terminated. Not the most elegant solution I'm sure, but it works...
 
 This project is licensed under the terms of the MIT license.
--- RothkoBot.py
+++ RothkoBot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import tweepy, time, sys, os, math, random, struct, string
 from PIL import Image, ImageDraw, ImageFilter
 
@@ -9,6 +8,22 @@ def touch(fname, times=None):
     with open(fname, 'a'):
         os.utime(fname, times)
 
+# Function to retrieve current tweet number from local file in case of program termination.
+def get_tweet_count():
+    tweet_count = open("tweet_count.txt", "r")
+    tweet_num = int(tweet_count.read())
+    tweet_count.close()
+    return tweet_num
+
+# Function to save the image that is to be tweeted and return the path of the image
+def save_image(tweet_number, image):
+    image_filename = "Rothko"+str(tweet_number)+".png"
+    image_path = os.getcwd()+"/Images/"+image_filename
+    touch(image_path)
+    image.save(image_path)
+    return image_path
+
+# Boilerplate code for using the Twitter API
 # Enter the corresponding information from your Twitter application:
 CONSUMER_KEY = os.environ['ROTHKO_API_KEY']        # Replace this with your consumer key or set an environmental variable
 CONSUMER_SECRET = os.environ['ROTHKO_API_SECRET']  # Replace this with your consumer secret key or set an environmental variable
@@ -18,59 +33,52 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-color = open("colors.txt", "r") 
-colors = color.read().splitlines()
-color.close()
+color_file = open("colors.txt", "r") 
+colors = color_file.read().splitlines()
+color_file.close()
 
-# Open .txt file to retrieve current number of tweets in case of program termination.
-TweetCount = open("TweetCount.txt", "r")
-TweetNumber = TweetCount.read()
-TweetNumber = int(TweetNumber)
-TweetCount.close()
+tweet_number = get_tweet_count()
 
 while True:
-    fourcolors = False
+    four_colors = False
     color1 = colors[random.randint(0,len(colors)-1)].split(",") # Select background color and name
     color2 = colors[random.randint(0,len(colors)-1)].split(",") # Select first rectangle color and name
     color3 = colors[random.randint(0,len(colors)-1)].split(",") # Select second rectangle color and name
     color4 = colors[random.randint(0,len(colors)-1)].split(",") # Secelect third rectangle color and name
     
-    colorstr1 = struct.unpack("BBB",color1[1][1:].decode("hex")) # Decode hex color values to (R,G,B) format 
-    colorstr2 = struct.unpack("BBB",color2[1][1:].decode("hex"))
-    colorstr3 = struct.unpack("BBB",color3[1][1:].decode("hex"))
-    colorstr4 = struct.unpack("BBB",color4[1][1:].decode("hex"))
+    color_str1 = struct.unpack("BBB",color1[1][1:].decode("hex")) # Decode hex color values to (R,G,B) format 
+    color_str2 = struct.unpack("BBB",color2[1][1:].decode("hex"))
+    color_str3 = struct.unpack("BBB",color3[1][1:].decode("hex"))
+    color_str4 = struct.unpack("BBB",color4[1][1:].decode("hex"))
     
     WIDTH,HEIGHT = 418,518
     length1 = random.randint(10,278)  # Determine lengths of each rectangle
     length2 = random.randint(0,(478-length1))
     length3 = 463-(length2+length1)
     
-    RothkoImage = Image.new('RGB', (WIDTH, HEIGHT))
-    draw = ImageDraw.Draw(RothkoImage)
-    draw.rectangle([0, 0, WIDTH, HEIGHT], fill=colorstr1, outline=colorstr1)
-    draw.rectangle([20, 20, WIDTH-20, length1+20], fill=colorstr2, outline=colorstr2)
-    draw.rectangle([20, length1+30, WIDTH-20, length2+length1+30], fill=colorstr3, outline=colorstr3)
+    rothko_image = Image.new('RGB', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(rothko_image)
+    draw.rectangle([0, 0, WIDTH, HEIGHT], fill=color_str1, outline=color_str1)
+    draw.rectangle([20, 20, WIDTH-20, length1+20], fill=color_str2, outline=color_str2)
+    draw.rectangle([20, length1+30, WIDTH-20, length2+length1+30], fill=color_str3, outline=color_str3)
+    
     if length3 > 0:
-        draw.rectangle([20, length1+length2+40, WIDTH-20, length1+length2+length3+40], fill=colorstr4, outline=colorstr4)
-        fourcolors = True
+        draw.rectangle([20, length1+length2+40, WIDTH-20, length1+length2+length3+40], fill=color_str4, outline=color_str4)
+        four_colors = True
 
-    # Save properly-named image to PNG file in the proper directory
-    RothkoPng = "Rothko"+str(TweetNumber)+".png"
-    RothkoPath = os.getcwd()+"/Images/"+RothkoPng 
-    touch(RothkoPath)
-    RothkoImage.save(RothkoPath)
+    rothko_path = save_image(tweet_number, rothko_image)
 
-    if fourcolors:
-        tweet = "No. " + str(TweetNumber) + " (" + color2[0].title() + ", " + color3[0].title() + ", and " + color4[0].title() + " on " + color1[0].title() + ")"
-        TweetNumber = TweetNumber + 1
+    if four_colors:
+        tweet = "No. " + str(tweet_number) + " (" + color2[0].title() + ", " + color3[0].title() + ", and " + color4[0].title() + " on " + color1[0].title() + ")"
+        tweet_number = tweet_number + 1
     else:
-        tweet = "No. " + str(TweetNumber) + " (" + color2[0].title() + " and " + color3[0].title() + " on " + color1[0].title() + ")"   
-        TweetNumber = TweetNumber + 1
+        tweet = "No. " + str(tweet_number) + " (" + color2[0].title() + " and " + color3[0].title() + " on " + color1[0].title() + ")"   
+        tweet_number = tweet_number + 1
 
     # Write current tweet count to file in case of termination
-    TweetCount = open("TweetCount.txt", "w")
-    TweetCount.write(str(TweetNumber))
-    TweetCount.close()
+    tweet_count = open("tweet_count.txt", "w")
+    tweet_count.write(str(tweet_number))
+    tweet_count.close()
 
-    api.update_with_media(RothkoPath, tweet) # Send tweet
-    time.sleep(7200) # Wait 1  hour before tweeting again
+    api.update_with_media(rothko_path, tweet) # Send tweet
+    time.sleep(7200) # Wait 2 hours before tweeting again
